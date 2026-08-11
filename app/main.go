d9ca485d5d10461346f3960572e2c58a23a574a3
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-08-11 21:58:59 +0100

    handle invalid commands

--- app/main.go
+++ app/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+
+	input := bufio.NewScanner(os.Stdin)
+
+	input.Scan()
+	fmt.Printf("%s: command not found", input.Text())
 }
